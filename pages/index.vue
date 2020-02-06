7744da69fe68b1092b1cbb0fc070de3e2808c68e
Pavel Prokopenko <nipplees8@gmail.com>
2020-02-07 01:02:06 +0300

    added main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <section>
- HELLO
-  </section>
+
+  <div class="myclass">
+    <i v-if="loadSpinner" class="flex-center fa fa-spin fa-spinner"></i>
+    <div class="row " v-if="departments.length">
+      <div class="col py-2 px-lg-2 border"></div>
+      <div class="col py-2 px-lg-2 border text-center" v-for="department in departments" :key="department.id">
+        {{department.name}}
+      </div>
+    </div>
+    <div class="row " v-for="employee in employees.data" :key="employee.id">
+      <div class="col py-2 px-lg-2 border"> {{employee.name}} {{employee.surname}}</div>
+      <div class="col py-2 px-lg-2 border" v-for="department in departments" :key="department.id">
+        <div v-for="dep in employee.departments" :key="dep.id" v-if="dep.id===department.id" class="text-center">
+          <i class="fas fa-check"></i>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
 </template>
-<!--
 
 <script>
   export default {
@@ -18,28 +34,15 @@
       this.fetch();
     },
     methods: {
-      fetch() {
+     async fetch() {
         this.loadSpinner = true;
-        Promise.all([Axios.get('../department'), Axios.get('../employee')]).
-            then(([departmentResponse, employeeResponse]) => {
-              this.departments = departmentResponse.data;
-              this.employees = employeeResponse.data;
-              this.loadSpinner = false;
-            }).
-            catch(error => {
-              console.log(error);
-            });
-
+       this.employees = await this.$axios.$get('http://127.0.0.1:8000/employee');
+       this.departments = await this.$axios.$get('http://127.0.0.1:8000/department');
+       this.loadSpinner = false;
       },
-      nextPageEmployees(page = 1) {
-        Axios.get('../employee?page=' + page).then(response => {
-          this.employees = response.data;
-        });
       },
-    },
   };
 </script>
--->
 
 <style scoped>
   .myclass {
