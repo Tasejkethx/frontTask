e43b7115ea585381264beeda5938470dbb3a955d
Pavel Prokopenko <nipplees8@gmail.com>
2020-02-06 23:21:12 +0300

    added inputForm component

---
+++ components/FormComponents/inputForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label :for="name" class="font-weight-bold"> {{label}} </label>
+    <input class="form-control" :type="type" :id="id" :name="name" v-bind:value="value"
+           v-on:input="$emit('update:value', $event.target.value)" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: String,
+      id: String,
+      label: String,
+      value: String,
+      type:String,
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
