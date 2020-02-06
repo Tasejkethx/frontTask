c472e5316836dd10a7503b15c3a29616819bb6e0
Pavel Prokopenko <nipplees8@gmail.com>
2020-02-06 23:22:10 +0300

    added checkboxForm component

---
+++ components/FormComponents/checkboxForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="mb-3 mt-3 font-weight-bold"> {{title}}</div>
+    <div class="custom-control custom-checkbox mb-1" v-for="checkButton in checkButtons"
+         :key="checkButton.id">
+      <input type="checkbox" class="custom-control-input" :id="checkButton.id" :value="checkButton.id"
+             v-on:change="onDepChange"
+             v-model="isSelected"
+      />
+      <label class="custom-control-label" :for="checkButton.id"> {{checkButton.name}}</label>
+    </div>
+    <span class="mb-2 mt-2" id="department_id"></span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: String,
+      checkButtons: Array,
+      selected: Array,
+    },
+    data() {
+      return {
+        isSelected: this.selected,
+      };
+    },
+    methods: {
+      onDepChange: function() {
+        this.$emit('onDepChange', this.isSelected);
+      },
+    },
+    watch: {
+      'selected': function(value) {
+        this.isSelected = value;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
